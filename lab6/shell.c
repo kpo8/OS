2e01303462df2fad091e7cb2f4d5931a6b8264e1
Aleks Drobnjak <ad155@zips.uakron.edu>
2019-04-09 20:24:46 -0400

    Fix ddir command

--- lab6/shell.c
+++ lab6/shell.c
@@ -66,44 +66,51 @@ void main()
 //DDIR funciton
 void listFiles()
 {
+
 	// 257 is where the directory is
-        int i =0;
-        int k = 0;
-        int getNameLength=0;
-        int q =8;
+    int i =0;
+    int k = 0;
+    int getNameLength=0;
+    int q = 0;
 	int fileSize=0;
 	int totalSize=0;
 	char dummy[16];
-        char bufferDirectory[512];
-        interrupt(33,2,bufferDirectory,257,0);
-       // Goes through all 16 files in directory
-        while(i < 512)
+    char bufferDirectory[512];
+    interrupt(33,2,bufferDirectory,257,0);
+
+   	// Goes through all 16 files in directory
+    while(i < 512)
+    {
+        // lists files 
+       	while(bufferDirectory[i+k] != '\0')
         {
-                // lists files 
-           	while(bufferDirectory[i+k] != '\0')
-                {
 			dummy[k] = bufferDirectory[i+k];
 			++k;
-                }
+        }
+
 		dummy[k] = '\0';		
-     		//lists sizes        
-     		while(q < 24)
+
+ 		//lists sizes        
+ 		while(q < 24)
 		{
 			int currentSector = bufferDirectory[i + 8 + q];
 
 			// Read each sector into buffer
-			if(currentSector == 0)
+			if(currentSector != 0)
 			{
 				// Updates the amounts of sectors requried by file
-				fileSize = q;
+				++fileSize;
+			}
+			else
+			{
 				break;
 			}
-		
+	
 			++q;                            
 		}
 
-		totalSize = fileSize + totalSize;	
-		
+		totalSize += fileSize;	
+	
 		if(dummy[0] != '\0')
 		{
 			PRINTS(dummy);
@@ -113,18 +120,18 @@ void listFiles()
 		}
 
 		//cleans up
-		k =0;
-		q =0;
-		fileSize =0;
-	        i += 32;
-        }
+		k = 0;
+		q = 0;
+		fileSize = 0;
+   		i += 32;
+    }
+
 	PRINTS("DISK USAGE: ");
 	PRINTN(totalSize);
 	PRINTS("\n\r\0");
 	PRINTS("FREE SPACE: ");
 	PRINTN(256-totalSize);
-	PRINTS("\n\r\0");
-}
+	PRINTS("\n\r\0");}
 
 int stringCompare(char one[10], char two[10])
 {
@@ -177,7 +184,7 @@ void terminalCommands(char *s)
 	switch (getOption(s)) 
 	{
 		case BOOT0:
-		     	BOOT;  			
+	     	BOOT;  			
 			break;
 		case CLRS1:
 			CLRS;	
@@ -186,7 +193,7 @@ void terminalCommands(char *s)
 			forCopy(s);
 			break;
 		case DDIR3:
-		        listFiles();
+	        listFiles();
 			PRINTS("\n\r\0");	
 			break;
 		case ECHO4:
@@ -202,24 +209,24 @@ void terminalCommands(char *s)
 			HELP;	
 			break;
 		case PRNT7:
-		        arg1(s);
+	        arg1(s);
 			PRNT(s);	
 			break;
 		case REMV8:
-		        arg1(s);
+	        arg1(s);
 			REMV(s);	
 			break;
 		case SENV9: 
 			SENV;
 			break;
 		case SHOW10:
-		        arg1(s);
+	        arg1(s);
 			SHOW(s);
 			break;
 		case TWET11:
-	//	       	PRINTS("ENTER TEXT\r\n\0");
-//			arg1(s);
-//			TWET(s);
+			//arg1(s);
+	     	//PRINTS("ENTER TEXT\r\n\0");
+			//TWET(s);
 			break;
 		case BADOPTION:
 			PRINTS("BAD OPTION \n\r\0");
